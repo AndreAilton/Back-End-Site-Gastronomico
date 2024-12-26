71ba97b258f89e8fff7111c3703e901663a72d9b
Andre <andre.ailtonf.10@gmail.com>
2024-12-25 22:46:14 -0300

    Projeto Final

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -1,13 +1,13 @@
 import OrderController from "../controllers/orderController.js";
 import express from "express";
-
+import authenticate  from "../middlewares/authenticate.js";;
 
 const ordersRouter = express.Router();
 
 ordersRouter.get("/", OrderController.getAllOrder);
 ordersRouter.get("/available", OrderController.getAvailableOrder);
 ordersRouter.get("/:id", OrderController.getOrderById);
-ordersRouter.get("/client/:id", OrderController.getOrderByClient);
+ordersRouter.get("/client/:id", authenticate, OrderController.getOrderByClient);
 ordersRouter.post("/", OrderController.createPlate);
 ordersRouter.delete("/:id", OrderController.deleteOrder);
 ordersRouter.put("/:id", OrderController.updateOrder);
